b924457394c6c333f10a1404f40a51977ba8926c
TheAyeshaSiddiqa <ayeshasiddiqatheprogrammer@gmail.com>
2020-10-16 03:13:35 +0500

    Updated API Code

--- app/api/enrolments.py
+++ app/api/enrolments.py
@@ -1,10 +1,31 @@
 from fastapi import APIRouter
 
+from app.repositories.s3_course_repo import S3CourseRepo
 from app.repositories.s3_enrolment_repo import S3EnrolmentRepo
+from app.requests.create_course_request import NewCourseRequest
 from app.requests.enrolment_requests import NewEnrolmentRequest
+from app.use_cases.create_course import CreateNewCourse
 from app.use_cases.create_new_enrolment import CreateNewEnrolment
 
 router = APIRouter()
+enrolment_repo = S3EnrolmentRepo()
+course_repo = S3CourseRepo()
+
+
+@router.post("/post_course")
+def post_course(inputs: NewCourseRequest):
+    """
+    The Employer pre-registers a new Enrolment so callbacks can be received
+    The assumption is that the employer generates a unique *enrolment_id*
+    in some source system they control (LMS, HRMS, etc),
+    and registers it with this microservice.
+    This allows the microservice to generate keys,
+    and prepare to receive callbacks from the training provider
+    about this enrolment.
+    """
+    use_case = CreateNewCourse(course_repo=course_repo)
+    response = use_case.execute(inputs)
+    return response
 
 
 @router.get("/enrolments/{enrolment_id}")
@@ -23,7 +44,6 @@ def create_enrolment(inputs: NewEnrolmentRequest):
 
     The initial state of the enrollment authorisation is “lodged”.
     """
-    enrolment_repo = S3EnrolmentRepo()
     use_case = CreateNewEnrolment(enrolment_repo=enrolment_repo)
     response = use_case.execute(inputs)
 
