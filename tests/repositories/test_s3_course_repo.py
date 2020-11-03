146f6c5f588f473939ef19ea29a5991ced4a5b2b
TheAyeshaSiddiqa <ayeshasiddiqatheprogrammer@gmail.com>
2020-11-03 13:05:27 +0500

    Review Changes

--- tests/repositories/test_s3_course_repo.py
+++ tests/repositories/test_s3_course_repo.py
@@ -12,7 +12,7 @@ from app.config import settings
 from app.repositories.s3_course_repo import S3CourseRepo
 from tests.test_data.course_data_provider import CourseDataProvider
 
-# from unittest.mock import mock.patch
+test_data = CourseDataProvider()
 
 
 @mock.patch("boto3.client")
@@ -29,15 +29,15 @@ def test_s3_initialisation(boto_client):
 def test_save_course(boto_client, get_uuid):
     repo = S3CourseRepo()
     settings.COURSE_BUCKET = "some-bucket"
-    course_id = CourseDataProvider().sample_course_id
-    request = CourseDataProvider().sample_create_course_request
+    course_id = test_data.sample_course_id
+    request = test_data.sample_create_course_request
 
-    get_uuid.return_value = CourseDataProvider().sample_course_id
-    with mock_datetime_now(CourseDataProvider().created, datetime):
+    get_uuid.return_value = test_data.sample_course_id
+    with mock_datetime_now(test_data.created, datetime):
         course = repo.create_course(request)
 
-    print(course, CourseDataProvider().sample_course)
-    assert course == CourseDataProvider().sample_course
+    print(course, test_data.sample_course)
+    assert course == test_data.sample_course
 
     boto_client.return_value.put_object.assert_called_once_with(
         Body=bytes(course.json(), "utf-8"),
@@ -49,11 +49,11 @@ def test_save_course(boto_client, get_uuid):
 @mock.patch("app.repositories.s3_course_repo.S3CourseRepo.get_course")
 @mock.patch("boto3.client")
 def test_update_course(boto_client, repo_get_course):
-    course_id = CourseDataProvider().sample_course_id
+    course_id = test_data.sample_course_id
     repo = S3CourseRepo()
     settings.COURSE_BUCKET = "some-bucket"
-    sample_course = CourseDataProvider().sample_course
-    params = CourseDataProvider().sample_update_course_request
+    sample_course = test_data.sample_course
+    params = test_data.sample_update_course_request
 
     repo_get_course.return_value = sample_course
     course = repo.update_course(params)
@@ -70,10 +70,10 @@ def test_update_course(boto_client, repo_get_course):
 @mock.patch("json.loads")
 @mock.patch("boto3.client")
 def test_get_course(boto_client, json_loads):
-    course_id = CourseDataProvider().sample_course_id
+    course_id = test_data.sample_course_id
     repo = S3CourseRepo()
     settings.COURSE_BUCKET = "some-bucket"
-    sample_course = CourseDataProvider().sample_course
+    sample_course = test_data.sample_course
 
     json_loads.return_value = sample_course.dict()
     course = repo.get_course(course_id)
