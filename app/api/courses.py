a861912ba599508f2f87274648dd1b92df38d8ac
TheAyeshaSiddiqa <ayeshasiddiqatheprogrammer@gmail.com>
2020-11-17 23:43:49 +0500

    added texts to all users and courses APIs and tested

--- app/api/courses.py
+++ app/api/courses.py
@@ -14,7 +14,26 @@ course_repo = S3CourseRepo()
 
 @router.post("/course")
 def post_course(inputs: NewCourseRequest, token: str = Depends(oauth2_scheme)):
-    """Create a new Course in Course Catalogue"""
+    """
+    Create a new Course in Course Catalogue\n
+    <b>course_name</b> is required.
+    <b>industry_standards</b> is a list of industry standard versions
+    this course confirms to.\n
+    <b>competency</b> is a list of competencies.\n
+    <b>location</b> is the location where course is conducted.
+    It is required.\n
+    <b>start_date</b> is in format ISO 8601 (YYYY-MM-DDTHH:mm:ss.sssZ).
+    It is the starting date of the course.
+    It is required.\n
+    <b>availability</b> is a boolean which is True or False depending
+    upon the availability of the course. It is required.\n
+    <b>hours_per_week</b> is the number of hours per week the course
+    demands from the students. It is a float value. It is required.\n
+    <b>duration</b> is the total duration of course in string.
+    It is required.\n
+    <b>fees_from</b> is the minimum fee of the course.
+    It is a float value. It is required\n
+    """
     use_case = cc.CreateNewCourse(course_repo=course_repo)
     response = use_case.execute(inputs)
     if bool(response) is False:  # If request failed
@@ -25,8 +44,25 @@ def post_course(inputs: NewCourseRequest, token: str = Depends(oauth2_scheme)):
 
 @router.put("/course")
 def update_course(inputs: UpdateCourseRequest, token: str = Depends(oauth2_scheme)):
-    """Update an existing Course in Course Catalogue
-    course id is mandatory, other attributes are optional
+    """Update an existing Course in Course Catalogue\n
+    <b>course_id</b> is mandatory which identifies the course.\n
+    <b>course_name</b> is optional.
+    <b>industry_standards</b> is a list of industry standard versions
+    this course confirms to. It is optional\n
+    <b>competency</b> is a list of competencies. It is optional\n
+    <b>location</b> is the location where course is conducted.
+    It is optional.\n
+    <b>start_date</b> is in format ISO 8601 (YYYY-MM-DDTHH:mm:ss.sssZ).
+    It is the starting date of the course.
+    It is optional.\n
+    <b>availability</b> is a boolean which is True or False depending
+    upon the availability of the course. It is optional.\n
+    <b>hours_per_week</b> is the number of hours per week the course
+    demands from the students. It is a float value. It is optional.\n
+    <b>duration</b> is the total duration of course in string.
+    It is optional.\n
+    <b>fees_from</b> is the minimum fee of the course.
+    It is a float value. It is optional\n
     """
     use_case = uc.UpdateCourse(course_repo=course_repo)
     response = use_case.execute(inputs)
@@ -38,8 +74,9 @@ def update_course(inputs: UpdateCourseRequest, token: str = Depends(oauth2_schem
 
 @router.get("/course/{course_id}")
 def get_course(course_id: str, token: str = Depends(oauth2_scheme)):
-    """Get an existing Course in Course Catalogue
-    using course id
+    """Get an existing Course in Course Catalogue.\n
+    This API returns the information of the course.\n
+    <b>course_id</b> is mandatory which identifies the course.\n
     """
     use_case = gc.GetCourseByID(course_repo=course_repo)
     response = use_case.execute(course_id)
